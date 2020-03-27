9f0de2dc4780f9e4426411fbf1c89d20fd9d2656
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-03-28 01:54:42 +0300

    Import "innfer If" block

--- assets/src/admin/script.js
+++ assets/src/admin/script.js
@@ -2,3 +2,32 @@
  * Block dependencies
  */
 import './style.css';
+import './utils/category';
+
+/**
+ * Import custom/built-in editor blocks.
+ */
+import * as innerIf from './if/';
+
+export function registerBlocks() {
+	[ 
+		innerIf 
+	].forEach( ( block ) => {
+		if ( ! block ) {
+			return;
+		} // End If Statement
+
+		const { name, category, icon, settings } = block;
+
+		wp.blocks.registerBlockType( `ifblocks/mypreview-${ name }`, {
+			category,
+			icon: {
+				src: icon,
+				foreground: '#007CBA',
+			},
+			...settings,
+		} );
+	} );
+}
+
+registerBlocks();
