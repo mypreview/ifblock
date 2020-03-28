96611191f4cc7caede4ef08df6cb8451a7a69230
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-03-28 22:26:23 +0300

    Generated

---
+++ languages/ifblock-translations.php
@@ -0,0 +1,23 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: assets/src/else/components/Edit.jsx:36
+	// Reference: assets/src/ifblock/components/Edit.jsx:40
+	_x( 'Content shown if the conditions of the if-block are not met.', 'help', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Edit.jsx:54
+	_x( 'End If Statement', 'help', 'ifblock' ),
+
+	// Reference: assets/src/else/index.js:23
+	_x( 'Else', 'block title', 'ifblock' ),
+
+	// Reference: assets/src/if/index.js:23
+	_x( 'If', 'block title', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/index.js:24
+	_x( 'If Block', 'block title', 'ifblock' ),
+
+	// Reference: assets/src/utils/category.js:22
+	_x( 'MyPreview', 'block category', 'ifblock' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
