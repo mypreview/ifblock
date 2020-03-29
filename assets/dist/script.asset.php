f18b5c4ae1bd1a3dec941b9c76a7908498152598
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-03-30 01:58:24 +0300

    Compiled

--- assets/dist/script.asset.php
+++ assets/dist/script.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '21862d1fc31356f46788c46f7a8d7eac');
+<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => 'c76570751e808b229b079af1dc5281d6');
