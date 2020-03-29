5a90c1ac259fcf4fd516bbb1a1e2a1a8654a0f68
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-03-29 21:50:56 +0300

    Added "Conditional operator" control

--- assets/src/ifblock/components/Inspector.jsx
+++ assets/src/ifblock/components/Inspector.jsx
@@ -10,7 +10,13 @@ const { _x } = wp.i18n;
 const { Fragment, Component } = wp.element;
 const { compose } = wp.compose;
 const { InspectorControls } = wp.blockEditor;
-const { PanelBody, SelectControl } = wp.components;
+const {
+	PanelBody,
+	BaseControl,
+	ButtonGroup,
+	Button,
+	SelectControl,
+} = wp.components;
 
 export default compose( applyWithSelect )(
 	class Inspector extends Component {
@@ -22,12 +28,80 @@ export default compose( applyWithSelect )(
 				roles,
 				browsers,
 			} = this.props;
-			const { role, browser } = attributes;
+			const { operator, role, browser } = attributes;
 
 			return (
 				<Fragment>
 					<InspectorControls>
 						<PanelBody initialOpen={ true }>
+							{ !! role && !! browser && (
+								<BaseControl
+									id={ `${ className }-conditional-operator` }
+									label={ _x(
+										'Operator',
+										'control label',
+										'ifblock'
+									) }
+									help={
+										'and' === operator
+											? _x(
+													'The block will be shown if all conditions defined below are true or match.',
+													'control label',
+													'ifblock'
+											  )
+											: _x(
+													'The block will be shown if either of the conditions defined below is true or a match.',
+													'control label',
+													'ifblock'
+											  )
+									}
+								>
+									<ButtonGroup
+										aria-label={ _x(
+											'Conditional operator',
+											'aria label',
+											'ifblock'
+										) }
+									>
+										<Button
+											isSmall
+											isPrimary={
+												'and' === operator
+													? true
+													: false
+											}
+											onClick={ () =>
+												setAttributes( {
+													operator: 'and',
+												} )
+											}
+										>
+											{ _x(
+												'AND',
+												'operator',
+												'ifblock'
+											) }
+										</Button>
+										<Button
+											isSmall
+											isPrimary={
+												'or' === operator ? true : false
+											}
+											onClick={ () =>
+												setAttributes( {
+													operator: 'or',
+												} )
+											}
+										>
+											{ _x(
+												'OR',
+												'operator',
+												'ifblock'
+											) }
+										</Button>
+									</ButtonGroup>
+								</BaseControl>
+							) }
 							<SelectControl
 								label={ _x(
 									'User Role',
