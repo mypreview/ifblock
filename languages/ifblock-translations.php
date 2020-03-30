21fe4210e375501b59f78059dddd717830fe4634
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-03-30 15:26:29 +0300

    Generated

--- languages/ifblock-translations.php
+++ languages/ifblock-translations.php
@@ -1,11 +1,16 @@
 <?php
 /* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
 $generated_i18n_strings = array(
-	// Reference: assets/src/else/components/Edit.jsx:36
-	// Reference: assets/src/ifblock/components/Edit.jsx:40
-	_x( 'Content shown if the conditions of the if-block are not met.', 'help', 'ifblock' ),
+	// Reference: assets/src/ifblock/components/Edit.jsx:110
+	__( 'Fetching Data%s', 'ifblock' ),
 
-	// Reference: assets/src/ifblock/components/Edit.jsx:54
+	// Reference: assets/src/else/components/Edit.jsx:41
+	_x( 'Content shown if the conditions of the above block are not met.', 'help', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Edit.jsx:136
+	_x( 'Mix and match different conditional statements from the right-hand sidebar panel.', 'help', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Edit.jsx:150
 	_x( 'End If Statement', 'help', 'ifblock' ),
 
 	// Reference: assets/src/else/index.js:23
@@ -16,6 +21,57 @@ $generated_i18n_strings = array(
 
 	// Reference: assets/src/ifblock/index.js:24
 	_x( 'If Block', 'block title', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Edit.jsx:78
+	_x( 'logged-in with the following role: %s', 'notice', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Edit.jsx:88
+	_x( 'visiting via %s', 'notice', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Edit.jsx:93
+	_x( 'Content shown to users that are %s %s', 'notice', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Inspector.jsx:106
+	_x( 'User Role', 'control label', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Inspector.jsx:125
+	_x( 'Browser', 'control label', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Inspector.jsx:40
+	_x( 'Operator', 'control label', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Inspector.jsx:47
+	_x( 'The block will be shown if all conditions defined below are true or match.', 'control label', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Inspector.jsx:52
+	_x( 'The block will be shown if either of the conditions defined below is true or a match.', 'control label', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Inspector.jsx:60
+	_x( 'Conditional operator', 'aria label', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Inspector.jsx:79
+	_x( 'AND', 'operator', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Inspector.jsx:96
+	_x( 'OR', 'operator', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Inspector.jsx:111
+	_x( 'The block content will be shown to users that are logged-in and in the selected role.', 'control help', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/components/Inspector.jsx:130
+	_x( 'The block content will be shown to users that are browsing the page via the selected browser.', 'control help', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/index.js:33
+	_x( 'This block enables you to configure certain content to appear only if specified conditions are met (or be hidden) by setting different visibility rules below.', 'block description', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/index.js:39
+	_x( 'Protected', 'block keyword', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/index.js:40
+	_x( 'Visibility', 'block keyword', 'ifblock' ),
+
+	// Reference: assets/src/ifblock/index.js:41
+	_x( 'Show Hide', 'block keyword', 'ifblock' ),
 
 	// Reference: assets/src/utils/category.js:22
 	_x( 'MyPreview', 'block category', 'ifblock' )
