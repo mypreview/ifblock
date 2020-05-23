0e4c42f53b6c16efdb7d777d2b263b36c590c64d
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-05-24 01:02:37 +0300

    Default `Babel` preset for WordPress development

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	presets: [ '@wordpress/babel-preset-default' ],
+	plugins: [
+		[
+			'@babel/plugin-proposal-class-properties',
+			{
+				loose: true,
+			},
+		],
+		[
+			'@babel/plugin-proposal-object-rest-spread',
+			{
+				loose: true,
+				useBuiltIns: true,
+			},
+		],
+	],
+};
