22a6d963a936ed9583fc8a27e77725e97489e235
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-05-24 01:01:51 +0300

    Added support for TerserPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 /**
  * All of the the JavaScript compile functionality
- * for Container Block plugin reside in this file.
+ * for `If Block` plugin reside in this file.
  *
  * @requires    Webpack
  * @package     ifblock
@@ -10,25 +10,27 @@ const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
 const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
 const ProgressBarPlugin = require( 'progress-bar-webpack-plugin' );
-const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+const TerserPlugin = require( 'terser-webpack-plugin' );
 const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
 const FixStyleOnlyEntriesPlugin = require( 'webpack-fix-style-only-entries' );
 const WebpackRTLPlugin = require( 'webpack-rtl-plugin' );
 const WebpackNotifierPlugin = require( 'webpack-notifier' );
 const chalk = require( 'chalk' );
+const package = 'If Block';
+const jsonp = 'webpackIfBlockJsonp';
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const editorConfig = {
 	entry: {
-		script: './assets/src/script.js',
+		script: './src',
 	},
 	output: {
-		path: path.resolve( __dirname, './assets/dist/' ),
+		path: path.resolve( __dirname, './dist/' ),
 		filename: '[name].js',
 		libraryTarget: 'this',
 		// This fixes an issue with multiple webpack projects using chunking
 		// See https://webpack.js.org/configuration/output/#outputjsonpfunction
-		jsonpFunction: 'webpackIfBlockJsonp',
+		jsonpFunction: jsonp,
 	},
 	mode: NODE_ENV,
 	performance: {
@@ -52,28 +54,15 @@ const editorConfig = {
 			{
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader?cacheDirectory',
-					options: {
-						presets: [ '@wordpress/babel-preset-default' ],
-						plugins: [
-							[ 'transform-class-properties' ],
-							[
-								'@babel/plugin-proposal-object-rest-spread',
-								{
-									loose: true,
-									useBuiltIns: true,
-								},
-							],
-							[
-								'@wordpress/babel-plugin-makepot',
-								{
-									output: 'languages/ifblock-js.pot',
-								},
-							],
-						],
+				use: [
+					require.resolve( 'thread-loader' ),
+					{
+						loader: require.resolve( 'babel-loader' ),
+						options: {
+							cacheDirectory: process.env.BABEL_CACHE_DIRECTORY || true,
+						},
 					},
-				},
+				],
 			},
 			{
 				test: /\.css$/,
@@ -88,9 +77,6 @@ const editorConfig = {
 					},
 					{
 						loader: 'postcss-loader',
-						options: {
-							plugins: [ require( 'autoprefixer' ) ],
-						},
 					},
 				],
 			},
@@ -102,16 +88,10 @@ const editorConfig = {
 		'window.jQuery': 'jquery',
 	},
 	optimization: {
+		minimize: true,
 		minimizer: [
-			new UglifyJsPlugin( {
-				cache: true,
-				parallel: true,
-				uglifyOptions: {
-					output: {
-						ie8: false,
-						comments: false,
-					},
-				},
+			new TerserPlugin( {
+				extractComments: false,
 			} ),
 		],
 	},
@@ -119,6 +99,7 @@ const editorConfig = {
 		new CleanWebpackPlugin(),
 		new BundleAnalyzerPlugin( {
 			openAnalyzer: false,
+			analyzerPort: 6000,
 		} ),
 		new FixStyleOnlyEntriesPlugin(),
 		new MiniCssExtractPlugin( {
@@ -129,16 +110,13 @@ const editorConfig = {
 		} ),
 		new ProgressBarPlugin( {
 			format:
-				chalk.blue( 'Build core script' ) +
-				' [:bar] ' +
-				chalk.green( ':percent' ) +
-				' :msg (:elapsed seconds)',
+				chalk.blue( 'Build core script' ) + ' [:bar] ' + chalk.green( ':percent' ) + ' :msg (:elapsed seconds)',
 		} ),
 		new DependencyExtractionWebpackPlugin( {
 			injectPolyfill: true,
 		} ),
 		new WebpackNotifierPlugin( {
-			title: 'If Block',
+			title: package,
 			alwaysNotify: true,
 			skipFirstNotification: true,
 		} ),
