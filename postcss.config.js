38b666ee324e5e23dbf312d1834cd1c7c5d4046e
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-05-24 00:48:40 +0300

    Added plugins for the `PostCSS` loader

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = ( { env } ) => ( {
+	plugins: {
+		'postcss-import': {},
+		'postcss-for': {},
+		'postcss-each': {},
+		'postcss-nested': {},
+		'postcss-css-variables': {},
+		'postcss-custom-media': {},
+		'postcss-calc': {},
+		'postcss-combine-media-query': {},
+		autoprefixer: {},
+	},
+} );
