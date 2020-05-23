14ab15ea98da8fb9b3db44459d74c3a31a8e6104
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-05-24 02:23:50 +0300

    Renamed

--- src/if/utils/with-select.js
+++ src/if/utils/with-select.js
@@ -9,7 +9,6 @@ const { withSelect } = wp.data;
  */
 const applyWithSelect = withSelect( ( select, { clientId } ) => {
 	const { getBlock } = select( 'core/block-editor' );
-
 	const block = getBlock( clientId );
 
 	return {
